9c1e79da564c6038214c6c1413df0b27b90c4b26
Favrou jérôme <38081092+jeromefavrou@users.noreply.github.com>
2019-10-31 15:15:20 +0100

    Update BasketLFPV2.ino
    code source binaire généré plus court 
    simple proposition code encoe ameliorable

--- BasketLFPV2.ino
+++ BasketLFPV2.ino
@@ -2,11 +2,12 @@
 #include <LiquidCrystal_I2C.h>
 #include <EEPROM.h>
 
-LiquidCrystal_I2C lcd(0x27, 16, 2);
-int highscore = 0;
-int valuescore;
-int leds = 4;
-int pinphoto = 0;
+#define slave_addr 0x27
+
+LiquidCrystal_I2C lcd(slave_addr, 16, 2);
+int highscore = 0,valuescore;
+
+int const leds{4}, pinphoto{0};
 
 void setup() {
 
@@ -17,48 +18,38 @@ void setup() {
   lcd.clear();
 
   pinMode(leds, OUTPUT);
-  delay(250);
+  delayMicroseconds(50);
+  
   digitalWrite(leds, LOW);
   delay(250);
-  digitalWrite(leds, HIGH);
-  delay(250);
-  digitalWrite(leds, LOW);
-  delay(250);
-  digitalWrite(leds, HIGH);
-  delay(250);
-  digitalWrite(leds, LOW);
-  delay(250);
-  digitalWrite(leds, HIGH);
-  delay(250);
-  digitalWrite(leds, LOW);
-  delay(250);
-  digitalWrite(leds, HIGH);
 
+  flash_led<leds,7>(4.0);
 }
 
-void loop() {
-
-  uint16_t baskets = 0;
-  uint16_t print_time = 0;
-  boolean in_hoop = false;
-  boolean start_game = false;
+void loop() 
+{
+  uint16_t baskets {0}, print_time{0};
+  bool in_hoop {false}, start_game {false};
+  
   lcd.setCursor(0, 0);
   lcd.print("BasketBall LFP");
   lcd.setCursor(0, 1);
   lcd.print("Lance la balle !");
 
   // Demarrer le jeu lorsque la balle passe
-  if (isBallInHoop()) {
-    start_game = true;
-  }
+  
+  start_game= isBallInHoop<pinphoto>() ? true : start_game;
 
   if (start_game) {
 
     // Temps = 1000 * 30s
-    for (uint16_t time = 30000; time > 0; time -= 10) {
+    for (uint16_t time = 30000; time > 0; time -= 10)
+    {
       // if time == an even second update matrix
-      if (time % 1000 == 0) {
+      if (time % 1000 == 0) 
+      {
         print_time = time / 1000;
+        
         lcd.clear();
         lcd.setCursor(0, 0);
         lcd.print("Temps:");
@@ -71,51 +62,28 @@ void loop() {
         lcd.setCursor(8, 1);
         lcd.print(baskets);
         lcd.cursor_off();
+        
         digitalWrite(leds, LOW);
 
       }
 
-      if (in_hoop) {
-        if (!isBallInHoop()) {
-          in_hoop = false;
-        }
-      }
 
-      if (!in_hoop && isBallInHoop()) {
+      in_hoop=in_hoop && !isBallInHoop<pinphoto>() ? false : in_hoop;
+
+
+      if (!in_hoop && isBallInHoop<pinphoto>()) 
+      {
         in_hoop = true;
         baskets++;
+        
         lcd.clear();
         lcd.setCursor(3, 0);
         lcd.print("BUT !!!!");
-        digitalWrite(leds, HIGH);
-        delay(50);
-        digitalWrite(leds, LOW);
-        delay(50);
-        digitalWrite(leds, HIGH);
-        delay(50);
-        digitalWrite(leds, LOW);
-        delay(50);
-        digitalWrite(leds, HIGH);
-        delay(50);
-        digitalWrite(leds, LOW);
-        delay(50);
-        digitalWrite(leds, HIGH);
-        delay(50);
-        digitalWrite(leds, LOW);
-        delay(50);
-        digitalWrite(leds, HIGH);
-        delay(50);
-        digitalWrite(leds, LOW);
-        delay(50);
-        digitalWrite(leds, HIGH);
-        delay(50);
-        digitalWrite(leds, LOW);
-        delay(50);
 
-         if (highscore < baskets) {
-        EEPROM.write(valuescore, baskets);
-      }
+        flash_led<leds,10>(20.0);
 
+        if (highscore < baskets) 
+          EEPROM.write(valuescore, baskets);
       }
 
      
@@ -123,24 +91,13 @@ void loop() {
     }
 
     highscore = EEPROM.read(valuescore);
-    digitalWrite(leds, HIGH);
-    delay(100);
-    digitalWrite(leds, LOW);
-    delay(100);
-    digitalWrite(leds, HIGH);
-    delay(100);
-    digitalWrite(leds, LOW);
-    delay(100);
-    digitalWrite(leds, HIGH);
-    delay(100);
-    digitalWrite(leds, LOW);
-    delay(100);
-    digitalWrite(leds, HIGH);
-    delay(100);
-    digitalWrite(leds, LOW);
+    
+    flash_led<leds,8>(10.0);
+
     lcd.clear();
 
-    if (highscore > baskets) {
+    if (highscore > baskets) 
+    {
       lcd.print("Score Max :");
       lcd.setCursor(13, 0);
       lcd.print(highscore);
@@ -150,8 +107,8 @@ void loop() {
       lcd.print(baskets);
 
     }
-
-    else {
+    else 
+    {
       lcd.setCursor(0, 0);
       lcd.print("Nouveau record !");
       lcd.setCursor(0, 1);
@@ -165,7 +122,7 @@ void loop() {
     
     }
     delay(100);
-  }
+}
 
 
   ///////////////////////////////////////////////////////
@@ -174,12 +131,22 @@ void loop() {
   // 
   ///////////////////////////////////////////////////////
   
-  boolean isBallInHoop() {
+template<int _pin> inline bool isBallInHoop(void) 
+{
+    return analogRead(_pin) <= 600 ? false : true ;
+}
 
-    if (analogRead(pinphoto) <= 600) {
-      
-      return false;
-    }
-    
-    return true; 
+template<int _pin,long n_puls> void flash_led(float const & freq)
+{
+  uint16_t inter=1000000.0/freq;
+  
+  digitalWrite(_pin, HIGH);
+  
+  for(auto i=1u; i < n_puls ; i++)
+  {
+    delayMicroseconds(inter);
+    digitalWrite(_pin, i%2 );
   }
+}
+
+
